1100c79b8a24d35ac449d9a9802515bce6c3356b
서수교 <sk.seo@navercorp.com>
2023-02-25 13:14:51 +0900

    2023-02-25 학습

--- 025_프로젝트_2/my-app/src/pages/Home/Home.jsx
+++ 025_프로젝트_2/my-app/src/pages/Home/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './home.css';
+import axios from "axios";
 
 const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts")
+            .then((json) => {
+               setPosts(json.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, []);
+
+    console.log(posts);
+
     return (
         <div>
-            
+            Home
+            {
+                posts.map(post => <h2 key={post.id}>{post.title}</h2>)
+            }
         </div>
     );
 };
